8babf6b328aebe35aa98dee5d67ce87056ac2c6b
Ahmed Raafat <ar.ud.fw@gmail.com>
2022-11-18 07:33:15 +0200

    add new endpoint (healthz, metrics) and logging

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,26 +1,28 @@
 import sqlite3
+import logging
 
-from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
-from werkzeug.exceptions import abort
+from flask import Flask, json, render_template, request, url_for, redirect, flash
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    app.config['db_count'] += 1
     return connection
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                        (post_id,)).fetchone()                    
     connection.close()
     return post
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+app.config['db_count']=0
 
 # Define the main route of the web application 
 @app.route('/')
@@ -36,13 +38,23 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      
+      app.logger.error('the post ID is not found a 404 page is shown')       
+      
       return render_template('404.html'), 404
+    
     else:
-      return render_template('post.html', post=post)
+        post_title = post['title']
+        app.logger.info(f'Post Title {post_title}')
+        
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    
+    app.logger.info("The About Us page is retrieved")
+    
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,10 +73,43 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.info(f"A new article is created. title:{title}")
+            
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    posts_count = len(posts)
+    connection.close()
+    
+    response = app.response_class(
+            response=json.dumps({"data":{"db_connection_count": app.config['db_count'], "post_count": posts_count}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+
 # start the application on port 3111
 if __name__ == "__main__":
+
+   logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
    app.run(host='0.0.0.0', port='3111')
